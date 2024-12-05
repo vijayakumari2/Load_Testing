901c5ab1bd2e495c20106afa7f4b119011cea144
vijaya <vijaya.kumari@appnetwise.com>
2024-12-05 22:53:46 +0530

    Test

--- grafana/config/stages.js
+++ grafana/config/stages.js
@@ -1,5 +1,8 @@
-stages: [
-    { duration: '5m', target: 50 },
-    { duration: '10m', target: 15 },
-    { duration: '5m', target: 0 },
-]
+export let options = { 
+    stages: [ 
+            { duration: '5m', target: 50 }, // Ramp-up to 500 users 
+            { duration: '10m', target: 15 }, // Hold steady at 500 users 
+            { duration: '5m', target: 0 },   // Ramp-down to 0 users 
+         ], 
+      maxRedirects: 4 };
+    
